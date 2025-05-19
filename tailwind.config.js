43cefb0dd4abd8869d6040d892435c20f2572ef8
Apoorva Bandi <bandiapoorvaa@gmail.com>
2025-05-19 18:59:25 -0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
-// tailwind.config.js
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-  "./public/index.html"
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        gradient: "gradientBG 12s ease infinite",
+      },
+      keyframes: {
+        gradientBG: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" }
+        }
+      }
+    }
+  },
+  plugins: [],
+}
